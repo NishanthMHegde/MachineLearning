d4265b94791daf1ee65cb554598bdbfbf7d2c824
nishanthmhegde <=>
2019-06-21 21:51:14 +0530

    Added DecisionTreeClassifier

---
+++ DecisionTreeClassification/DecisionTreeClassifier.py
@@ -0,0 +1,85 @@
+"""
+Decision Tree Classifier splits the data horizontally and vertically and uses
+the independent variables to construct a tree, where each of the branches are
+either Yes/No until a suitable classification is obtained where most/all of
+the points belong to the category similar to the new ata point which is being added.
+"""
+import numpy as np 
+import pandas as pd 
+import matplotlib.pyplot as plt
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.cross_validation import train_test_split
+from sklearn.preprocessing import StandardScaler 
+from sklearn.metrics import confusion_matrix 
+
+df = pd.read_csv("Social_Network_Ads.csv")
+#Let us use age and salary as independent variables
+
+X = df.iloc[:, [2, 3]].values
+y = df.iloc[:, 4].values 
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.30, random_state=0)
+
+#We do not need to feature scale age and salary since the model does not relyon Euclidean distance.
+#But let us feature scale for now because we are plotting with a high resolution 
+scaler_X = StandardScaler()
+X_train = scaler_X.fit_transform(X_train)
+X_test = scaler_X.transform(X_test)
+#Let us now build our model 
+#DecisionTreeClassifier takes a paramter criterion where we can specify entropy to use information gain
+classifier = DecisionTreeClassifier(criterion="entropy", random_state=0)
+classifier = classifier.fit(X_train, y_train)
+
+#Now let us predict the predictor variable values present in the test set
+print("Now let us predict the predictor variable values present in the test set")
+y_pred = classifier.predict(X_test)
+print(y_pred)
+#Let us now compare with the actual training data results
+print("Let us now compare with the actual training data results")
+print(y_test)
+
+#We can see that it is difficult to compare predicted data with real test data
+#Let us use confusion matrix to make the comparison simpler 
+print("We can see that it is difficult to compare predicted data with real test data")
+print("Let us use confusion matrix to make the comparison simpler")
+cm = confusion_matrix(y_test, y_pred)
+print("The confusion_matrix results are:")
+print(cm)
+
+#We can summarize it by plotting the classification
+print("We can summarize it by plotting the classification")
+print("we can see that there are a lot of classification patches because of large number of splits.")
+# Visualising the Training set results
+from matplotlib.colors import ListedColormap
+X_set, y_set = X_train, y_train
+X1, X2 = np.meshgrid(np.arange(start = X_set[:, 0].min() - 1, stop = X_set[:, 0].max() + 1, step = 0.01),
+                     np.arange(start = X_set[:, 1].min() - 1, stop = X_set[:, 1].max() + 1, step = 0.01))
+plt.contourf(X1, X2, classifier.predict(np.array([X1.ravel(), X2.ravel()]).T).reshape(X1.shape),
+             alpha = 0.75, cmap = ListedColormap(('red', 'green')))
+plt.xlim(X1.min(), X1.max())
+plt.ylim(X2.min(), X2.max())
+for i, j in enumerate(np.unique(y_set)):
+    plt.scatter(X_set[y_set == j, 0], X_set[y_set == j, 1],
+                c = ListedColormap(('red', 'green'))(i), label = j)
+plt.title('DecisionTreeClassifier (Training set)')
+plt.xlabel('Age')
+plt.ylabel('Estimated Salary')
+plt.legend()
+plt.show()
+
+# Visualising the Test set results
+from matplotlib.colors import ListedColormap
+X_set, y_set = X_test, y_test
+X1, X2 = np.meshgrid(np.arange(start = X_set[:, 0].min() - 1, stop = X_set[:, 0].max() + 1, step = 0.01),
+                     np.arange(start = X_set[:, 1].min() - 1, stop = X_set[:, 1].max() + 1, step = 0.01))
+plt.contourf(X1, X2, classifier.predict(np.array([X1.ravel(), X2.ravel()]).T).reshape(X1.shape),
+             alpha = 0.75, cmap = ListedColormap(('red', 'green')))
+plt.xlim(X1.min(), X1.max())
+plt.ylim(X2.min(), X2.max())
+for i, j in enumerate(np.unique(y_set)):
+    plt.scatter(X_set[y_set == j, 0], X_set[y_set == j, 1],
+                c = ListedColormap(('red', 'green'))(i), label = j)
+plt.title('DecisionTreeClassifier (Test set)')
+plt.xlabel('Age')
+plt.ylabel('Estimated Salary')
+plt.legend()
+plt.show()
