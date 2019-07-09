d1af0a2a1bdf196ee1235a2a1cefdaba388db5e9
nishanthmhegde <=>
2019-07-09 21:44:37 +0530

    Adding LDA

---
+++ LinearDiscriminantAnalysis/LDA.py
@@ -0,0 +1,97 @@
+"""
+
+Linear Disciminant Analysis is also used to reduce the number of dimensions of the dataset.
+However, unlike the PCA, LDA is a supervised algorithm because it takes into account and uses
+the dependent variable. The goal of the LDA is to find out to what extent the independent
+variables can be used to classify the dependent variable value into seperate classes.
+PCA found out the extent to which the independent variables maximized the variance,
+whereas LDA looks at how much the independent variable can seperate the data into
+clusters.
+
+LDA almost always has a 100% accuracy rate because we choose independent variables
+which can BEST classify the data.
+
+"""
+import numpy as np 
+import pandas as pd 
+import math 
+import matplotlib.pyplot as plt
+from sklearn.linear_model import LogisticRegression
+from sklearn.cross_validation import train_test_split
+from sklearn.preprocessing import StandardScaler 
+from sklearn.metrics import confusion_matrix 
+from sklearn.discriminant_analysis import LinearDiscriminantAnalysis as LDA 
+
+df = pd.read_csv("Wine.csv")
+X = df.iloc[:, 0:13].values
+y = df.iloc[:, 13].values 
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=0)
+#Apply LDA here
+#We select 2 components here because we want to project them to a 2D subspace
+lda = LDA(n_components=2)
+#We need to fit LDA model using independent as well as dependent variable data
+X_train = lda.fit_transform(X_train, y_train)
+X_test = lda.transform(X_test)
+scaler_X = StandardScaler()
+X_train = scaler_X.fit_transform(X_train)
+X_test = scaler_X.transform(X_test)
+
+#Let us now build our model 
+classifier = LogisticRegression(random_state=0)
+classifier = classifier.fit(X_train, y_train)
+
+#Now let us predict the predictor variable values present in the test set
+print("Now let us predict the predictor variable values present in the test set")
+y_pred = classifier.predict(X_test)
+print(y_pred)
+#Let us now compare with the actual training data results
+print("Let us now compare with the actual training data results")
+print(y_test)
+
+#We can see that it is difficult to compare predicted data with real test data
+#Let us use confusion matrix to make the comparison simpler 
+print("We can see that it is difficult to compare predicted data with real test data")
+print("Let us use confusion matrix to make the comparison simpler")
+cm = confusion_matrix(y_test, y_pred)
+print("The confusion_matrix results are:")
+print(cm)
+#We can see that we have 100% accurate results
+print("We can see that we have 100% accurate results")
+#We can summarize it by plotting the classification
+print("We can summarize it by plotting the classification")
+
+# Visualising the Training set results
+from matplotlib.colors import ListedColormap
+X_set, y_set = X_train, y_train
+X1, X2 = np.meshgrid(np.arange(start = X_set[:, 0].min() - 1, stop = X_set[:, 0].max() + 1, step = 0.01),
+                     np.arange(start = X_set[:, 1].min() - 1, stop = X_set[:, 1].max() + 1, step = 0.01))
+plt.contourf(X1, X2, classifier.predict(np.array([X1.ravel(), X2.ravel()]).T).reshape(X1.shape),
+             alpha = 0.75, cmap = ListedColormap(('red', 'green', 'blue')))
+plt.xlim(X1.min(), X1.max())
+plt.ylim(X2.min(), X2.max())
+for i, j in enumerate(np.unique(y_set)):
+    plt.scatter(X_set[y_set == j, 0], X_set[y_set == j, 1],
+                c = ListedColormap(('red', 'green', 'blue'))(i), label = j)
+plt.title('Logistic Regression (Training set)')
+plt.xlabel('LD1')
+plt.ylabel('LD2')
+plt.legend()
+plt.show()
+
+# Visualising the Test set results
+from matplotlib.colors import ListedColormap
+X_set, y_set = X_test, y_test
+X1, X2 = np.meshgrid(np.arange(start = X_set[:, 0].min() - 1, stop = X_set[:, 0].max() + 1, step = 0.01),
+                     np.arange(start = X_set[:, 1].min() - 1, stop = X_set[:, 1].max() + 1, step = 0.01))
+plt.contourf(X1, X2, classifier.predict(np.array([X1.ravel(), X2.ravel()]).T).reshape(X1.shape),
+             alpha = 0.75, cmap = ListedColormap(('red', 'green', 'blue')))
+plt.xlim(X1.min(), X1.max())
+plt.ylim(X2.min(), X2.max())
+for i, j in enumerate(np.unique(y_set)):
+    plt.scatter(X_set[y_set == j, 0], X_set[y_set == j, 1],
+                c = ListedColormap(('red', 'green', 'blue'))(i), label = j)
+plt.title('Logistic Regression (Test set)')
+plt.xlabel('LD1')
+plt.ylabel('LD2')
+plt.legend()
+plt.show()
