dffc7deefc725797384a9e3172c4ac3edd9d456b
nishanthmhegde <=>
2019-07-11 21:43:44 +0530

    Adding Kfold cross validation

---
+++ KfoldCrossValidation/KFoldCrossValidation.py
@@ -0,0 +1,102 @@
+"""
+K-fold cross validation is a technique in which partition the data into k sub samples randomly
+and test out each of these sub-samples and find out their accuracies and return the accuracies.
+We can then use the mean of these accuracies to decide on how accurate our model is because
+just running the classifier once will not give us an idea on how good our classifier model
+is. It might be good for some set of data and it might be bad for some subsets or combinations
+of data points. To take into account all the bias and variance, we run the K-Fold Cross
+Validation. 
+
+It is used for model selection where we can select the best accurate model from a list or
+bucket of all possible classification models.
+"""
+
+import numpy as np 
+import pandas as pd 
+import matplotlib.pyplot as plt
+from sklearn.svm import SVC
+from sklearn.cross_validation import train_test_split
+from sklearn.model_selection import cross_val_score
+from sklearn.preprocessing import StandardScaler 
+from sklearn.metrics import confusion_matrix 
+
+df = pd.read_csv("Social_Network_Ads.csv")
+#Let us use age and salary as independent variables
+
+X = df.iloc[:, [2, 3]].values
+y = df.iloc[:, 4].values 
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.25, random_state=0)
+
+#We need to feature scale age and salary since the model does not automatically take care of it.
+scaler_X = StandardScaler()
+X_train = scaler_X.fit_transform(X_train)
+X_test = scaler_X.transform(X_test)
+
+#Let us now build our model 
+#you can play around with the linear, polynomial and rbf Kernels
+classifier = SVC(kernel='rbf', random_state=0)
+classifier = classifier.fit(X_train, y_train)
+#LEt us apply K-fold cross validation to predict how accuracte our dataset is
+#for all random subsets of our dataset.
+print("Let us apply K-fold cross validation to predict how accuracte our dataset is"
+	  " for all random subsets of our dataset. ")
+
+#Here estimator is the classifier model and cv is the number of cross fold validations
+accuracies = cross_val_score(estimator= classifier, X=X_train, y=y_train, cv=10)
+print("The list of accuracies are %s"%(accuracies))
+print("The mean of accuracies is %s"%(accuracies.mean()))
+print("The standard deviation of accuracies is %s"%(accuracies.std()))
+#Now let us predict the predictor variable values present in the test set
+print("Now let us predict the predictor variable values present in the test set")
+y_pred = classifier.predict(X_test)
+print(y_pred)
+#Let us now compare with the actual training data results
+print("Let us now compare with the actual training data results")
+print(y_test)
+
+#We can see that it is difficult to compare predicted data with real test data
+#Let us use confusion matrix to make the comparison simpler 
+print("We can see that it is difficult to compare predicted data with real test data")
+print("Let us use confusion matrix to make the comparison simpler")
+cm = confusion_matrix(y_test, y_pred)
+print("The confusion_matrix results are:")
+print(cm)
+
+#We can summarize it by plotting the classification
+print("We can summarize it by plotting the classification")
+
+# Visualising the Training set results
+from matplotlib.colors import ListedColormap
+X_set, y_set = X_train, y_train
+X1, X2 = np.meshgrid(np.arange(start = X_set[:, 0].min() - 1, stop = X_set[:, 0].max() + 1, step = 0.01),
+                     np.arange(start = X_set[:, 1].min() - 1, stop = X_set[:, 1].max() + 1, step = 0.01))
+plt.contourf(X1, X2, classifier.predict(np.array([X1.ravel(), X2.ravel()]).T).reshape(X1.shape),
+             alpha = 0.75, cmap = ListedColormap(('red', 'green')))
+plt.xlim(X1.min(), X1.max())
+plt.ylim(X2.min(), X2.max())
+for i, j in enumerate(np.unique(y_set)):
+    plt.scatter(X_set[y_set == j, 0], X_set[y_set == j, 1],
+                c = ListedColormap(('red', 'green'))(i), label = j)
+plt.title('SVM (Training set)')
+plt.xlabel('Age')
+plt.ylabel('Estimated Salary')
+plt.legend()
+plt.show()
+
+# Visualising the Test set results
+from matplotlib.colors import ListedColormap
+X_set, y_set = X_test, y_test
+X1, X2 = np.meshgrid(np.arange(start = X_set[:, 0].min() - 1, stop = X_set[:, 0].max() + 1, step = 0.01),
+                     np.arange(start = X_set[:, 1].min() - 1, stop = X_set[:, 1].max() + 1, step = 0.01))
+plt.contourf(X1, X2, classifier.predict(np.array([X1.ravel(), X2.ravel()]).T).reshape(X1.shape),
+             alpha = 0.75, cmap = ListedColormap(('red', 'green')))
+plt.xlim(X1.min(), X1.max())
+plt.ylim(X2.min(), X2.max())
+for i, j in enumerate(np.unique(y_set)):
+    plt.scatter(X_set[y_set == j, 0], X_set[y_set == j, 1],
+                c = ListedColormap(('red', 'green'))(i), label = j)
+plt.title('SVM (Test set)')
+plt.xlabel('Age')
+plt.ylabel('Estimated Salary')
+plt.legend()
+plt.show()
